5bd363d67f33bf880a2d20ad38a06fff58202bda
PoomNattapoom <rockyaggi@gmail.com>
2023-09-25 16:38:45 +0700

    cc

--- src/app/api/enrollment/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-if (foundCourseNo && foundStudentId) {
-  return NextResponse.json(
-    {
-      ok: false,
-      message: "You already registered this course",
-    },
-    { status: 400 }
-  );
-}
